f23985036115c2bbbf3cf4a1401463c8bd95164f
miinsuu <myson0545@naver.com>
2019-01-19 15:10:05 +0900

    Nodejs

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs=require('querystring');
 
-function templateHTML(title,list,body){
+function templateHTML(title,list,body,control){
   return `
   <!doctype html>
   <html>
@@ -13,6 +14,7 @@ function templateHTML(title,list,body){
   <body>
     <h1><a href="/">WEB2</a></h1>
     ${list}
+    ${control}
     ${body}
   </body>
   </html>
@@ -34,7 +36,6 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname=url.parse(_url, true).pathname;
-
     if(pathname==='/'){
       if(queryData.id===undefined){
 
@@ -42,7 +43,7 @@ var app = http.createServer(function(request,response){
             var title='Welcome';
             var description='Hello, Node.js';
             var list=templateList(filelist);
-            var template=templateHTML(title,list,`<h2>${title}</h2>${description}`);
+            var template=templateHTML(title,list,`<h2>${title}</h2>${description}`,`<a href="/create">create</a>`);
             response.writeHead(200);
             response.end(template);
           });
@@ -53,12 +54,44 @@ var app = http.createServer(function(request,response){
         fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
           var title=queryData.id;
           var list=templateList(filelist);
-          var template=templateHTML(title,list,`<h2>${title}</h2>${description}`);
+          var template=templateHTML(title,list,`<h2>${title}</h2>${description}`,`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
           response.writeHead(200);
           response.end(template);
         });
       });
       }
+    }else if(pathname==='/create'){
+      fs.readdir('./data',function(err,filelist){
+        var title='WEB - create';
+        var list=templateList(filelist);
+        var template=templateHTML(title,list,`
+          <form action="http://localhost:3000/create_process" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+          </form>
+          `,'');
+        response.writeHead(200);
+        response.end(template);
+      });
+    }else if(pathname==='/create_process'){
+      var body='';
+      request.on('data',function(data){
+        body=body+data;
+      });
+      request.on('end',function(){
+        var post=qs.parse(body);
+        var title=post.title;
+        var description=post.description;
+        fs.writeFile(`data/${title}`,description,'utf8',function(err){
+          response.writeHead(302,{Location: `/?id=${title}`});
+          response.end('success');
+        });
+      });
     }else{
         response.writeHead(404);
         response.end('Not found');
